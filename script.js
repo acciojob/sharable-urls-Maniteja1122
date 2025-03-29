70aa80fb316e74f11b0e4d267eff2b1b1aa1a233
MANITEJA KUNTA <129951439+Maniteja1122@users.noreply.github.com>
2025-03-29 23:09:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // your code here
+let NameInp = document.getElementById("name") 
+let yearInp = document.getElementById("year")
+let form = document.querySelector("form")
+let display = document.getElementById("url")
+
+form.addEventListener("submit" , (e)=>{
+	e.preventDefault()
+    display.innerText =  `https://localhost:8080/?name=${NameInp.value}&year=${yearInp.value}`
+	
+})
